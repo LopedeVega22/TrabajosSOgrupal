d654a9e6d1296c94cbb25230373e7edbbc280823
Felix Tejedor Zapatero <felteza@gmail.com>
2026-02-05 15:30:05 +0100

    Script con placeholders
    Script con placeholders

--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+if [ "$#" == "0" ] ;then
 #Datos de red del equipo
 ip add
 #Estado del servicio
 systemctl status apache2
+fi
 #Menu de instalacion
+while [ "$#" == "0" ]
+do
 while [[ "$Menu1" != "Salir" ]]
 do
 read -p "Opciones del menu del servicio:
@@ -12,7 +16,7 @@ read -p "Opciones del menu del servicio:
     - Poner en marcha el servicio (Escriba: Correr)
     - Parar el servicio (Escriba: Parada)
     - Consultar los logs (Escriba: Logs)
-    - Salir (Escriba: Salir)
+    - Salir (Escriba: contr + C)
     Escriba lo que desea hacer: " Menu1
 #Menu descarga
 case $Menu1 in
@@ -28,8 +32,6 @@ case $Menu1 in
             ;;
             "Docker")
             echo "Esto es para instalar con docker"
-            ;;
-            "Salir")
             ;;
             *)
                 echo "El valor no es valido"
@@ -101,4 +103,41 @@ case $Menu1 in
     ;;
 esac
 done
+done
+if [ "$1" == "-install" ];then
+ if [ "$2" == "-c" ];then
+  echo "Placeholder instalación con comandos"
+ elif [ "$2" == "-d" ];then
+  echo "Placeholder instalación con Docker"
+ else
+  echo "Esa opción no es válida, si necesita ayuda utilice --help al ejecutar el programa"
+ fi
+elif [ "$1" == "-delete" ];then
+ echo "Placeholder eliminación del servicio"
+elif [ "$1" == "-start" ];then
+ echo "Placeholder puesta en marcha"
+elif [ "$1" == "-stop" ];then
+ echo "Placeholder parada"
+elif [ "$1" == "-logs" ];then
+ echo "Placeholder consultar logs"
+elif [ "$1" == "--help" ];then
+ echo "------------------------------------------------------------------------------------"
+ echo "-install Instalación del servicio con comandos (-install -c) o Docker (-install -d)"
+ echo "------------------------------------------------------------------------------------"
+ echo "-delete Eliminación del servicio"
+ echo "------------------------------------------------------------------------------------"
+ echo "-start Puesta en marcha"
+ echo "------------------------------------------------------------------------------------"
+ echo "-stop Parada"
+ echo "------------------------------------------------------------------------------------"
+ echo "-logs Consultar logs:"
+ echo "-f por fecha"
+ echo "-t por tipo"
+ echo "Incluir la fecha(YYYY-MM-DD) o el tipo como tercer parámetro"
+ echo "------------------------------------------------------------------------------------"
+else
+echo ""
+echo "Esa opción no es válida, si necesita ayuda utilice --help al ejecutar el programa"
+echo ""
+fi
 
